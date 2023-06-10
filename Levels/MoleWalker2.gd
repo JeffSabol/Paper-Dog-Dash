1d902bcc5d170e952b8704315c6be2dfc9693353
JeffSabol <jeffreysabol345@gmail.com>
2023-06-09 20:06:34 -0400

    Adding walking mole enemies, hurt player state and crawl_hit animation

---
+++ Levels/MoleWalker2.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+@onready var path_follower = get_parent()
+@onready var _animated_sprite = $AnimatedSprite2D
+
+func _physics_process(_delta):
+	_animated_sprite.play("default")
+	
+	path_follower.progress += 1 # moves the mole along the path
+	
+	# conditionals flipped because he is going in the opposite direction
+	if path_follower.progress > 175:
+		$AnimatedSprite2D.flip_h = true 
+	elif path_follower.progress < 175:
+		$AnimatedSprite2D.flip_h = false 
+		
+func _on_body_entered(body):
+	if body is Player:
+		body.hurt()
