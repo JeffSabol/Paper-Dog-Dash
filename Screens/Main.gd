65dfcd4b962846c2d28730d093021d4f5bc1d5d1
Richard <iwannaplaygamesjp@gmail.com>
2023-06-14 20:35:02 -0400

    setting main as staring scene

--- Screens/Main.gd
+++ Screens/Main.gd
@@ -1,11 +1,16 @@
 extends Node2D
 
+@export var MainMenu: PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	start_main_menu()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func start_main_menu():
+	var main_menu = MainMenu.instantiate()
+	add_child(main_menu)
