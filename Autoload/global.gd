779c09b2b731da295ffa8d5a556549fb7abd64af
JeffSabol <jeffreysabol345@gmail.com>
2024-03-05 22:29:27 -0500

    Added type hints and a cleaner conditional for set_difficulty()

--- Autoload/global.gd
+++ Autoload/global.gd
@@ -8,35 +8,35 @@ enum DifficultyLevel { EASY, MEDIUM, HARD }
 # Hard : Enemies one hit the player. Only 1 attempt per level.
 
 # Lives
-var total_lives = 9999 # Default value that is overridden by set_difficulty()
+var total_lives: int = 9999 # Default value that is overridden by set_difficulty()
 
 # Level changing
 var current_scene = null
-var level_count = 2 # We want to change to level 2 after level 1
-var level_path = "res://Levels/level_" + str(level_count) + ".tscn"
+var level_count: int = 2 # We want to change to level 2 after level 1
+var level_path: String = "res://Levels/level_" + str(level_count) + ".tscn"
 
 # Newspaper handling
-var has_newspaper = false
+var has_newspaper: bool = false
 
 # Bone handling
-var total_bones = 0
+var total_bones: int = 0
 
 # Time handling
-var total_time = 170 
+var total_time: int = 170 
 
 # Difficulty handling
-var current_difficulty = DifficultyLevel.EASY
+var current_difficulty: int = DifficultyLevel.EASY
 
-func _ready():
+func _ready() -> void:
 	var root = get_tree().root
 	current_scene = root.get_child(root.get_child_count() - 1)
 
-func goto_scene(path):
+func goto_scene(path: String) -> void:
 	# Deleting the current scene at this point is
 	# a bad idea, because it may still be executing code.
 	call_deferred("_deferred_goto_scene",path)
 
-func _deferred_goto_scene(path):
+func _deferred_goto_scene(path: String):
 	# It is now safe to remove the current scene
 	current_scene.queue_free()
 
@@ -52,12 +52,12 @@ func _deferred_goto_scene(path):
 	# Optionally, to make it compatible with the SceneTree.change_scene_to_file() API.
 	get_tree().current_scene = current_scene
 
-func goto_next_level():
+func goto_next_level() -> void:
 	# Deleting the current scene at this point is
 	# a bad idea, because it may still be executing code.
 	call_deferred("_deferred_goto_next_level")
 
-func _deferred_goto_next_level():
+func _deferred_goto_next_level() -> void:
 	# It is now safe to remove the current scene
 	current_scene.queue_free()
 
@@ -77,14 +77,16 @@ func _deferred_goto_next_level():
 	level_path = "res://Levels/level_" + str(level_count) + ".tscn"
 	has_newspaper = false
 
-func set_difficulty(new_difficulty: int):
+func set_difficulty(new_difficulty: int) -> void:
 	current_difficulty = new_difficulty
-	if(current_difficulty == DifficultyLevel.EASY):
-		total_lives = 9999 # infinite lives
-	if(current_difficulty == DifficultyLevel.MEDIUM):
-		total_lives = 3
-	if(current_difficulty == DifficultyLevel.HARD):
-		total_lives = 1
+	
+	match current_difficulty:
+		DifficultyLevel.EASY:
+			total_lives = 9999 # infinite lives
+		DifficultyLevel.MEDIUM:
+			total_lives = 3
+		DifficultyLevel.HARD:
+			total_lives = 1
 
-func get_difficulty() -> bool:
+func get_difficulty() -> int:
 	return current_difficulty
