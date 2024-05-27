b54dea023eca2157f2471eecb25651f083e92d42
JeffSabol <jeffreysabol345@gmail.com>
2024-05-27 17:03:50 -0400

    Added settings menu when in-game

--- Menus/pause_menu.gd
+++ Menus/pause_menu.gd
@@ -1,5 +1,6 @@
 extends Control
 
+var settings_menu = null;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,3 +11,16 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func restore_focus():
+	$VBoxContainer/ResumeButton.grab_focus()
+
+func _on_settings_button_pressed():
+	$ButtonSound.play()
+	settings_menu = load("res://Menus/SettingsMenu.tscn").instantiate()
+	var new_scale = Vector2(0.375, 0.375)
+	settings_menu.set_scale(new_scale)
+	settings_menu.set_position(Vector2(64,80))
+	# Place the settings menu on top of any sprites
+	settings_menu.z_index = 1
+	add_child(settings_menu)
