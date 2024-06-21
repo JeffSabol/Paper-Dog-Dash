989ea879eabcb37c1f7eb4d787ec8359ce2e15ed
JeffSabol <jeffreysabol345@gmail.com>
2024-06-21 00:27:24 -0400

    Implemented John Conway - Doomsday Algorithm

--- Menus/EndOfLevel.gd
+++ Menus/EndOfLevel.gd
@@ -11,7 +11,6 @@ func _ready():
 	print(Time.get_datetime_dict_from_system(false))
 	get_datetime()
 	
-	print(Global.level_count) 
 	# level count will be level 2 because we already beat it
 	match (Global.level_count):
 		2:
@@ -46,14 +45,67 @@ func get_datetime():
 	var month: int = datetime["month"]
 	var day: int = datetime["day"]
 	var hour: int = datetime["hour"]
-	print(str(day))
+	dooms_day_algorithm(year, month, day)
 	# Morning = 6am to noon
 	# Afternoon = noon - 6pm
 	# Evening = 6pm - 9pm
 	# Night = 9pm - 6am
 
-func format_time_of_day(hour: int):
+func dooms_day_algorithm(year: int, month: int, day: int):
 	# John Conaway Doom's Day Algorithm
-	# TODO
-	#return ""
-	pass
+	# https://www.youtube.com/watch?v=z2x3SSBVGJU
+
+	# Calculate the anchor day for the century
+	var century = (year / 100) * 100
+	var anchor_day = (5 * ((century / 100) % 4) + 2) % 7
+	
+	# Calculate the doomsday for the year
+	var year_of_century = year % 100
+	var leap_years = year_of_century / 4
+	var doomsday = (year_of_century + leap_years + anchor_day) % 7
+	
+	# Calculate the doomsday for the month
+	var month_doomsdays = {}
+	if is_leap_year(year):
+		month_doomsdays[1] = 4
+		month_doomsdays[2] = 29
+	else:
+		month_doomsdays[1] = 3
+		month_doomsdays[2] = 28
+
+	month_doomsdays[3] = 14 # Pi Day
+	month_doomsdays[4] = 4
+	month_doomsdays[5] = 9
+	month_doomsdays[6] = 6
+	month_doomsdays[7] = 11
+	month_doomsdays[8] = 8
+	month_doomsdays[9] = 5 # I work 9 to 5
+	month_doomsdays[10] = 10
+	month_doomsdays[11] = 7 # At 7/11
+	month_doomsdays[12] = 12
+
+	var doomsday_for_month = month_doomsdays[month]
+	
+	# Calculate the day of the week
+	var day_of_week = (day - doomsday_for_month + doomsday) % 7
+	if day_of_week < 0:
+		day_of_week += 7  # Make the result non-negative
+
+	# Return the result as a day name
+	print(day_name(day_of_week))
+	return day_name(day_of_week)
+
+func calculate_leap_years(currentYear: int) -> int:
+	var leap_years_counter = 0
+	# currentYear+1 because currentYear could be a leap year
+	for year in range(2001, currentYear + 1):
+		if is_leap_year(year):
+			leap_years_counter += 1
+	return leap_years_counter
+
+func is_leap_year(year: int) -> bool:
+	return (year % 4 == 0) and ((year % 100 != 0) or (year % 400 == 0))
+
+func day_name(day: int) -> String:
+	var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+	return days[day]
