305485f754ad67ccaa77436a8f79742a53af7ef7
JeffSabol <jeffreysabol345@gmail.com>
2024-08-07 23:08:29 -0400

    used restore_focus method in pause_menu.gd

--- Menus/pause_menu.gd
+++ Menus/pause_menu.gd
@@ -4,8 +4,7 @@ var settings_menu = null;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var resume_button = get_node("VBoxContainer/ResumeButton")
-	resume_button.grab_focus()
+	restore_focus()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
