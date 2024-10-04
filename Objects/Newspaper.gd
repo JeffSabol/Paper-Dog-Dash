27029b61378680cf3e7933240e6a16511c7bddb0
JeffSabol <jeffreysabol345@gmail.com>
2024-10-03 23:59:34 -0400

    Added position smoothing to the camera. Restructured the node order so that the HUD is under the camera via Canvaslayer. Rather than under the player.

--- Objects/Newspaper.gd
+++ Objects/Newspaper.gd
@@ -9,7 +9,7 @@ func _on_body_entered(body):
 			$Newspaper.play()
 			var root = get_tree().root
 			var current_scene = root.get_child(root.get_child_count() - 1)
-			var hud = current_scene.get_node("Player/HUD")
+			var hud = current_scene.get_node("Camera2D/CanvasLayer/HUD")
 			hud.show_newspaper_text()
 			await get_tree().create_timer(1.0).timeout
 			queue_free() 
